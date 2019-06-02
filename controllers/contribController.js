d1bb9c557f74cd68cfe90a4350181f0ce558e157
kkopp <kristinakopp@Kristinas-MacBook-Pro.local>
2019-06-02 15:55:40 -0600

    added code for controller

---
+++ controllers/contribController.js
@@ -0,0 +1,38 @@
+const db = require("../models");
+
+// Defining methods for the booksController
+module.exports = {
+  findAll: function(req, res) {
+    db.Contrib
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Contrib
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    console.log('This is what exactly is going in',req.body);
+    db.Contrib
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Contrib
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Contrib
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
