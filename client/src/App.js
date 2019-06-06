641d66fd753dc7bcac3b6408735b95d26b144fa2
kkopp <kristina.kopp@gmail.com>
2019-06-05 22:12:56 -0600

    Created navbar, search box, footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './components/Header';
 import NavBar from './components/NavBar';
 import SearchBar from './components/SearchBar';
 import Footer from './components/Footer';
@@ -7,7 +6,6 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <Header />
       <NavBar />
       <SearchBar />
       <Footer />
